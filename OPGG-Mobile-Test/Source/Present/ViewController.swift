6285ae80160a9023aa7edf71002bbe2403a15073
taewoo <qkqnrpa@gmail.com>
2022-07-02 17:27:32 +0900

    프로젝트 기본 설정 및 폴더링

---
+++ OPGG-Mobile-Test/Source/Present/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OPGG-Mobile-Test
+//
+//  Created by 진태우 on 2022/07/02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .systemBrown
+    }
+
+
+}
+
